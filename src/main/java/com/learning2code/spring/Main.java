0a4eed9591cc7c9689120ef37ca29948adc92094
Cechina Denis <Cechina.Denis@gmail.com>
2020-11-26 19:02:32 +0200

    Created a bean with prototype scope + List bean injection Closes #3

--- src/main/java/com/learning2code/spring/Main.java
+++ src/main/java/com/learning2code/spring/Main.java
@@ -17,10 +17,17 @@ public class Main {
 //        MusicPlayer musicPlayer = new MusicPlayer(music);
 //      --> ~(IoC) Spring first of all creates musicBean than creates and injects it to musicPlayer bean
 //              depending what bean name we will use vvvvvvvvvvvvvvvv that bean will be got.
-        MusicPlayer player = context.getBean("musicPlayerSetterWithProperty", MusicPlayer.class);
-        player.playMusic();
-        System.out.println(player.getName());
-        System.out.println(player.getVolume());
+        MusicPlayer playerSingleton1 = context.getBean("musicPlayerConstructorListArgs", MusicPlayer.class);
+        MusicPlayer playerSingleton2 = context.getBean("musicPlayerConstructorListArgs", MusicPlayer.class);
+        playerSingleton1.playAllMusic();
+        System.out.println(playerSingleton1.getName());
+        System.out.println(playerSingleton1.getVolume());
+        System.out.println("Singleton scope enable; 2 obj gotten from the context are the same? -> "
+                + (playerSingleton1 == playerSingleton2));
+        MusicPlayer player1 = context.getBean("musicPlayerConstructorAllArgsPrototype", MusicPlayer.class);
+        MusicPlayer player2 = context.getBean("musicPlayerConstructorAllArgsPrototype", MusicPlayer.class);
+        System.out.println("Prototype scope enable; 2 obj gotten from the context are the same? -> "
+                + (player1 == player2));
 
         context.close();
     }
